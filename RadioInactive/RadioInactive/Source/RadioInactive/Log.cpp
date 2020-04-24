bc37341297ad547509237a0d1e36bf97c5791ac3
Anmol Kumar <azvenuskestel19@outlook.com>
2020-04-24 15:07:35 +0530

    .

---
+++ RadioInactive/RadioInactive/Source/RadioInactive/Log.cpp
@@ -0,0 +1,12 @@
+#include "Log.h"
+
+namespace RadioInactive
+{
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+	void Log::Init()
+	{
+
+	}
+}
---
+++ RadioInactive/RadioInactive/Source/RadioInactive/Log.h
@@ -0,0 +1,20 @@
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace RadioInactive
+{
+	class RI_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
