764ff2e2e3269b0ffb6f1fa05d3969817624ac5a
Anmol Kumar <azvenuskestel19@outlook.com>
2020-04-26 15:26:38 +0530

    .

--- RadioInactive/premake5.lua
+++ RadioInactive/premake5.lua
@@ -1,1 +1,23 @@
 workspace "RadioInactive"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hazel"
+	location "RadioInactive"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+
+	RI_PLATFORM_WINDOWS;RI_BUILD_DLL;_WINDLL;
