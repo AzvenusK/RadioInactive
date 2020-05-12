13262f061e7cd044d3fb382e400cc6848d477ea7
Anmol Kumar <azvenuskestel19@outlook.com>
2020-05-12 15:16:06 +0530

    .

--- RadioInactive/RadioInactive/KeyEvent.h
+++ RadioInactive/RadioInactive/KeyEvent.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace RadioInactive
+{
+	class RI_API KeyEvent : public Event
+	{
+	public:
+		inline int GetKeyode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+	protected:
+		KeyEvent(int KeyCode)
+			: m_KeyCode(keycode) {}
+
+		int m_KeyCode;
+	};
+
+	class RI_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+
+	};
+}
