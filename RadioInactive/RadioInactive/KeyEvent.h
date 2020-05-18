df2dbf87a3a9a032fef152a0fd76230e42697c96
Anmol Kumar <azvenuskestel19@outlook.com>
2020-05-18 15:58:00 +0530

    .

--- RadioInactive/RadioInactive/KeyEvent.h
+++ RadioInactive/RadioInactive/KeyEvent.h
@@ -34,10 +34,6 @@ namespace RadioInactive
 			return ss.str();
 		}
 
-		static	EventType	GetStsaticType() { return EventType::KeyPressed; }\
-		virtual EventType	GetEventType()		const override	{return GetStaticType();}\
-		virtual	const char*	GetName()			const override	{return "KeyPressed";}
-
 		EVENT_CLASS_TYPE(KeyPressed)
 	private:
 		int m_RepeatCount;
@@ -56,6 +52,6 @@ namespace RadioInactive
 			return ss.str();
 		}
 
-
+		EVENT_CLASS_TYPE(KeyReleased)
 	};
 }
