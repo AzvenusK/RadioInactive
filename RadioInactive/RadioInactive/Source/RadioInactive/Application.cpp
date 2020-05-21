85160024b96fae021d49cbb990e2f1119aa62770
Anmol Kumar <azvenuskestel19@outlook.com>
2020-05-21 16:05:56 +0530

    .

--- RadioInactive/RadioInactive/Source/RadioInactive/Application.cpp
+++ RadioInactive/RadioInactive/Source/RadioInactive/Application.cpp
@@ -1,5 +1,6 @@
 #include "Application.h"
-
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
 
 namespace RadioInactive
 {
@@ -15,6 +16,17 @@ namespace RadioInactive
 
 	void Application::Run()
 	{
+		
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			RI_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			RI_TRACE(e);
+		}
+
 		while (true);
 	}
 }
