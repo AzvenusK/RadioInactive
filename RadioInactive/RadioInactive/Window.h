549434a5854c17d57821a0eacff80fc776a9c78d
Anmol Kumar <azvenuskestel19@outlook.com>
2020-05-25 19:35:07 +0530

    .

---
+++ RadioInactive/RadioInactive/Window.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Source/RIPCH.h"
+
+#include "RadioInactive/Core.h"
+#include "RadioInactive/Events/Event.h"
+
+namespace RadioInactive
+{
+	struct WindowProps
+	{
+		std::string Title;
+		unsigned int Width;
+		unsigned int Height;
+
+		WindowProps(const std::string& title = "Radio Inactive Emgine",
+					unsigned int  width = 1280,
+					unsigned int height =  720)
+			: Title(title), Width(width), Height(height)
+		{
+		}
+	};
+
+	//Interfacing representing a desktop system based Window
+	class RI_API Window
+	{
+	public:
+		using EventCallbackFn = std::function<void(Event&)>;
+
+		virtual ~Window() {}
+
+		virtual void OnUpdate() = 0;
+
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		//Window attributes
+		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+		virtual void SetVSync(bool enabled) = 0;
+		virtual bool IsVSync() const = 0;
+
+		static Window* Create(const WindowProps& props = WindowProps());
+	};
+}
