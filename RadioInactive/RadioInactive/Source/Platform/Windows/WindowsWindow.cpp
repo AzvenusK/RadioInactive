9506c69a5d6b1fbe963cf22f5eaf0c7e5d3e4f25
Anmol Kumar <azvenuskestel19@outlook.com>
2020-06-16 18:19:57 +0530

    .

--- RadioInactive/RadioInactive/Source/Platform/Windows/WindowsWindow.cpp
+++ RadioInactive/RadioInactive/Source/Platform/Windows/WindowsWindow.cpp
@@ -1,6 +1,10 @@
 #include "RIPCH.h"
 #include "WindowsWindow.h"
 
+#include "RadioInactive/Events/ApplicationEvent.h"
+#include "RadioInactive/Events/KeyEvent.h"
+#include "RadioInactive/Events/MouseEvent.h"
+
 namespace RadioInactive
 {
 	static bool s_GLFWInitialized = false;
@@ -41,6 +45,75 @@ namespace RadioInactive
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
+
+		//Set GLFW Callbacks
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+				data.Width = width;
+				data.Height = height;
+
+				WindowResizeEvent event(width, height);
+				data.EventCallback(event);
+				
+			}
+		);
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+				WindowCloseEvent event;
+				data.EventCallback(event);
+			}
+		);
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int modes) 
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				switch (action)
+				{
+					case GLFW_PRESS:
+					{
+						KeyPressedEvent event(key, 0);
+						data.EventCallback(event);
+						break;
+					}
+					case GLFW_RELEASE:
+					{
+						KeyReleasedEvent event(key);
+						data.EventCallback(event);
+						break;
+					}
+					case GLFW_REPEAT:
+					{
+						KeyPressedEvent event(key, 1);
+						data.EventCallback(event);
+						break;
+					}
+				}
+			});
+
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int modes) 
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				switch (action)
+				{
+					case GLFW_PRESS:
+					{
+						MouseButtonPressedEvent event(button);
+						data.EventCallback(event);
+						break;
+					}
+					case GLFW_RELEASE:
+					{
+						MouseButtonReleasedEvent event(button);
+						data.EventCallback(event);
+						break;
+					}
+				}
+			});
 	}
 
 	void WindowsWindow::ShutDown()
--- RadioInactive/RadioInactive/Source/RadioInactive/Application.cpp
+++ RadioInactive/RadioInactive/Source/RadioInactive/Application.cpp
@@ -7,14 +7,22 @@
 
 namespace RadioInactive
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+	
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create()); 
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		RI_CORE_INFO("{0}", e);
 	}
 
 	void Application::Run()
